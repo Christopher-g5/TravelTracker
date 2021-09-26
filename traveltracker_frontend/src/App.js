3c4ca58fb584653a075801183aefd1d9126032a5
Christopher-g5 <c10gilmore@gmail.com>
2021-09-26 16:22:34 -0700

    Added login component

--- traveltracker_frontend/src/App.js
+++ traveltracker_frontend/src/App.js
@@ -1,14 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Login from './components/login';
+import LoginForm from './components/LoginForm';
+import React, {useState} from "react";
 
 function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+  };
+
+  const Logout = () => {
+    console.log("Logout");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Login></Login> 
-      </header>
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login ={Login} error={error} />
+      )}
     </div>
   );
 }
