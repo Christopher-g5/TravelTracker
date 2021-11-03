9d9f2830183456815957ffa777f014d3b5dc3949
Christopher-g5 <c10gilmore@gmail.com>
2021-11-02 20:53:52 -0700

    Airport AutoCorrect added

--- traveltracker_frontend/src/components/NewFlight.jsx
+++ traveltracker_frontend/src/components/NewFlight.jsx
@@ -1,10 +1,32 @@
 import React, { useState } from "react";
+import { Helmet } from "react-helmet";
 
 function NewFlight() {
+  const [details, setDetails] = useState({ depart: "", arrive: "" });
+
+  const submitHandler = async function (event) {
+    event.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <form onSubmit={submitHandler}>
+      <h2>Flight Information</h2>
+      <div className="form_group">
+        <div className="form-group">
+          <label htmlFor="depart">Departing Airport: </label>
+          <input
+            type="text"
+            name="depart"
+            id="autocomplete-airport-1"
+            onChange={(e) => setDetails({ ...details, depart: e.target.value })}
+          />
+          <Helmet>
+            <script src="https://cdn.jsdelivr.net/npm/airport-autocomplete-js@latest/dist/index.browser.min.js"></script>
+            <script>AirportInput("autocomplete-airport-1")</script>
+          </Helmet>
+        </div>
+      </div>
+    </form>
   );
 }
 
