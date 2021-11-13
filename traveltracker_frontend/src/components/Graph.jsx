57c9e660c5a8d9b5d573724e827b61924398c320
Christopher-g5 <c10gilmore@gmail.com>
2021-11-12 16:07:19 -0800

    API communication added

--- traveltracker_frontend/src/components/Graph.jsx
+++ traveltracker_frontend/src/components/Graph.jsx
@@ -1,8 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, Component, useEffect } from "react";
 import NewFlight from "./NewFlight";
+import axios from "axios";
+import cors from "cors";
 
 function Graph() {
-  return <div></div>;
+  useEffect(() => {
+    const api = "https://http.cat/401";
+    const data = {
+      title: "Black",
+      description:
+        "Black coffee is as simple as it gets with ground coffee beans steeped in hot water, served warm. And if you want to sound fancy, you can call black coffee by its proper name: cafe noir.",
+      ingredients: ["Coffee"],
+      id: 1,
+    };
+    axios
+      .post(api)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Graph</h1>
+    </div>
+  );
 }
 
 export default Graph;
--- traveltracker_frontend/src/components/NewFlight.jsx
+++ traveltracker_frontend/src/components/NewFlight.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import Graph from "./Graph";
 
 function NewFlight(uid) {
   //const [details, setDetails] = useState({ depart: "", arrive: "" });
@@ -31,39 +32,42 @@ function NewFlight(uid) {
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <Helmet async={true}>
-        <script defer={true}>
-          AirportInput("autocomplete-airport-1");
-          AirportInput("autocomplete-airport-2");
-        </script>
-      </Helmet>
-      <h2>Flight Information</h2>
-      <div className="form_group">
-        <div className="form-group">
-          <label htmlFor="depart">Departing Airport IATA Code: </label>
-          <input
-            type="text"
-            name="depart"
-            id="autocomplete-airport-1"
-            className="inp"
-            ref={refDepart}
-          />
-          <label htmlFor="date">Departure Date: </label>
-          <DatePicker selected={dates} onChange={(date) => setDates(date)} />
+    <div>
+      <form onSubmit={submitHandler}>
+        <Helmet async={true}>
+          <script defer={true}>
+            AirportInput("autocomplete-airport-1");
+            AirportInput("autocomplete-airport-2");
+          </script>
+        </Helmet>
+        <h2>Flight Information</h2>
+        <div className="form_group">
+          <div className="form-group">
+            <label htmlFor="depart">Departing Airport IATA Code: </label>
+            <input
+              type="text"
+              name="depart"
+              id="autocomplete-airport-1"
+              className="inp"
+              ref={refDepart}
+            />
+            <label htmlFor="date">Departure Date: </label>
+            <DatePicker selected={dates} onChange={(date) => setDates(date)} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="arrive">Destination Airport IATA Code: </label>
+            <input
+              type="text"
+              name="depart"
+              id="autocomplete-airport-2"
+              ref={refArrive}
+            />
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="arrive">Destination Airport IATA Code: </label>
-          <input
-            type="text"
-            name="depart"
-            id="autocomplete-airport-2"
-            ref={refArrive}
-          />
-        </div>
-      </div>
-      <input type="submit" value="Submit" />
-    </form>
+        <input type="submit" value="Submit" />
+      </form>
+      <Graph></Graph>
+    </div>
   );
 }
 
