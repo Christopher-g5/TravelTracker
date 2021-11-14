56173f03e9a72ddcea39bbde9dd6a011da23a6d0
Christopher-g5 <c10gilmore@gmail.com>
2021-11-13 16:05:14 -0800

    List creation added

--- traveltracker_frontend/src/components/TrackedFlights.jsx
+++ traveltracker_frontend/src/components/TrackedFlights.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import NewFlight from "./NewFlight";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function TrackedFlights(uid) {
   const [showNewFlightPage, setNewFlightVisibility] = useState(false);
+  const [refresh, setRefresh] = useState(false);
 
   const handleNewFlightClick = () => {
     setNewFlightVisibility(true);
   };
+
+  const fetchTrackedFlights = () => {
+    return null;
+  };
+
+  const data = ["Hello", "There", "Friend"];
 
   return (
     <div>
@@ -15,6 +23,12 @@ function TrackedFlights(uid) {
       ) : (
         <div>
           <h1>Tracked Flights</h1>
+          <div>
+            {fetchTrackedFlights}
+            {data.map((item) => (
+              <ListGroup.Item>{item}</ListGroup.Item>
+            ))}
+          </div>
           <button onClick={handleNewFlightClick}>+ Track a New Flight</button>
         </div>
       )}
