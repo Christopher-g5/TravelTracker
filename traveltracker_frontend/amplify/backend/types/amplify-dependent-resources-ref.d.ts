db648e53faa22b1c344b6d7a72064f960c8e0cbb
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2021-11-14 23:54:36 -0800

    put back amplify/cli.json

---
+++ traveltracker_frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "travelAPI": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "traveltrackertravelt45017c36": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
